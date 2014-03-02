28a63fe2c2bf6316ef37f3abecac1575c4951314
pthorod <preben.thorod@hotmail.com>
2014-03-02 23:43:35 +0100

    Fixed coefficient order in all transferfunctions. Added Root Locus Plot. Splitted generate and plot functionality

--- shelving/eq_testbench1.m
+++ shelving/eq_testbench1.m
@@ -7,20 +7,24 @@
 % and Zölner calculations and formulas for biquad filters.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% TODO:
+% Coeff control for testbench(testbench1 first)
+
 %% Return values:
 %   a = [  1, a1, a2];
 %   b = [ b0, b1, b2];
 
-%% System and analyzis parameters
-Fs = 41000;     %Hz
-Fsw = Fs*2*pi;  %sample rate rad/s
-Ts=1/Fs;
-
-%Testbench options
+%% Testbench options
 N = 1024;       %fft window size for freq analysis
 single = 0;     % Single(1) or multiple filter curves and coefficients
 bodegen = 1;    % Bodeplot(1) or freqz (0)
-FreqScale = 'linear'; %'linear' or 'log'
+rootlocus = 1;  % Show root locus plot
+FreqScale = 'log'; %'linear' or 'log'
+
+%% System parameters
+Fs = 41000;     %Hz
+Fsw = Fs*2*pi;  %sample rate rad/s
+Ts=1/Fs;
 
 %% Filter specification/ input parameters
 fc = 1000;       % Cutoff frequency
@@ -29,14 +33,6 @@ Gs = -12;        % Single coeffisient set, gain in dB
 Gm = [-12 0 6];
 Q = 0.8;                  % Q-factor
 type = 'Base_Shelf';      % 'Base_Shelf' or 'Treble_Shelf'
-
-%% 0226 : Fixed a,b mixup in single, not fixed in multiple
-
-% fc and G is dependent, if fc = 500, and G = 12  "damping_band = -12db
-% until 500Hz, then magnitude is -12db+3db  
-% Need to check out this logic closer!!!! --> Related to wrong order of a
-% and b! :D
-
 
 %% SINGLE COEFFISIENT SET
 if single == 1 
@@ -48,40 +44,52 @@ if single == 1
         tfd = tf(b,a,Ts);   %Discrete transfer function 
         filt(b,a)           %Print transfer function on DSP-format
         hbode = bodeplot(tfd);  
-        setoptions(hbode,'FreqUnits', 'Hz','FreqScale', FreqScale, 'Xlim',[1 Fs/2]);
+        setoptions(hbode,'FreqUnits', 'Hz','FreqScale', FreqScale, 'Xlim',[10 Fs/2]);
         sos = [b a]         % SOS 1x6 Matrix for simulink model
     else
         %freqz plot
         freqz(a,b,N,Fs); %verify that this a,b input parameter makes sense, "works, but not mentioned in doc"
+    end    
+    if rootlocus == 1
+         figure
+         rlocus(b,a)     
     end
-    
 %% MULTIPLE COEFFISIENT SET
-else 
-    %%a,b order broken!
-    B = cell(1,length(Gm));
-    A = cell(1,length(Gm));
-    H = cell(1,length(Gm));
-    tfd = cell(1,length(Gm));
-    if bodegen == 1
-        %bodeplot
+else
+    % Generate coefficients and transferfunctions:
+    N_coeff = length(Gm);
+    B = cell(1,N_coeff);
+    A = cell(1,N_coeff);
+    H = cell(1,N_coeff);
+    tfd = cell(1,N_coeff);
+    for i = 1:length(Gm)        
+        [B{i}, A{i}] = shelving(Gm(i), fc, Fs, Q, 'Base_Shelf');        
+        tfd{i} = tf(B{i},A{i},Ts);
+    end      
+    % Plot:
+    if bodegen == 1    
+        % Bode plot
         for i = 1:length(Gm)
-            % Generate coefficients 
-            [B{i}, A{i}] = shelving(Gm(i), fc, Fs, Q, 'Base_Shelf');        
-            %tfd = tf(A{i},B{i},Ts);
-            tfd = tf(B{i},A{i},Ts);
             hold on;
-            hbode = bodeplot(tfd);
-            setoptions(hbode,'FreqUnits', 'Hz','FreqScale', FreqScale, 'Xlim',[0 Fs/2]); %doc plotoptions
+            hbode = bodeplot(tfd{i});
+            setoptions(hbode,'FreqUnits', 'Hz','FreqScale', FreqScale, 'Xlim',[10 Fs/2]); %doc plotoptions
         end
     else
         %freqz plot
         for i = 1:length(Gm)
-            % Generate coefficient vector
-            [B{i}, A{i}] = shelving(Gm(i), fc, Fs, Q, 'Base_Shelf');            
             hold on;            
             freqz(A{i},B{i},N,Fs);            
         end         
     end 
+    
+    if rootlocus == 1
+        figure
+        for i = 1:N_coeff 
+            rlocus(B{i},A{i})  
+            hold on;           
+        end
+        hold off;
+    end
 end 
 
 
