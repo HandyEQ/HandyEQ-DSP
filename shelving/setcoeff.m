0111421f25334e087f54e1fd13f2a3339821982d
pthorod <preben.thorod@hotmail.com>
2014-03-14 03:18:56 +0100

    Added 3band testbench/generator for matlab

--- shelving/setcoeff.m
+++ shelving/setcoeff.m
@@ -1,11 +1,11 @@
 % Function for setting sending filter coefficients to Simulink testbench:
-% "singleBiquadTestbench2"
+% "BiquadTestbench_1band"
 % Preben Thorød - HandyEQ - Chalmers University of Technology 2014
 
 function [ ] = setcoeff( coeffIndex, acoeff, bcoeff, gainlist  )
-    set_param('singleBiquadTestbench2/B_coeff','Value',mat2str(bcoeff{coeffIndex}'));
-    set_param('singleBiquadTestbench2/A_coeff','Value',mat2str(acoeff{coeffIndex}(2:3)'));
-    set_param('singleBiquadTestbench2/Gain','Value',num2str(gainlist(coeffIndex)));
+    set_param('BiquadTestbench_1band/B_coeff','Value',mat2str(bcoeff{coeffIndex}'));
+    set_param('BiquadTestbench_1band/A_coeff','Value',mat2str(acoeff{coeffIndex}(2:3)'));
+    set_param('BiquadTestbench_1band/Gain','Value',num2str(gainlist(coeffIndex)));
     
 end
 
